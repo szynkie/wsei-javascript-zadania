c96e8c832db33ef26a32b27919e4b317d4335438
Dominik Suski <dominiksuski@MacBook-Dominik.local>
2020-10-11 17:59:31 +0200

    01 done

---
+++ 1.Początki/script.js
@@ -0,0 +1,19 @@
+//Zadanie 1
+console.log("Dominik Suski");
+// Dominik Suski
+
+//Zadanie 2
+console.log(12 + 32);
+//44
+console.log(52 / 16);
+//3.25
+console.log(5 * 2);
+//10
+
+//Zadanie 3
+console.log("Hello World");
+//Hello World
+//console.log(Hello World);
+//syntax
+//console.log("Hello World);
+//syntax
