61d836fb0b63bf3896ec10dc9d8a82a9e7a7b685
Dominik Suski <dominiksuski@MacBook-Dominik.local>
2020-10-16 14:38:02 +0200

    02 done

---
+++ 2. Zmienne i stałe/script.js
@@ -0,0 +1,32 @@
+//Zadanie 1
+const num = 1,
+str = '2',
+combo = num + str,
+bool = true,
+nothing = null;
+
+console.log(num);
+//1
+console.log(str);
+//2
+console.log(combo);
+//12
+console.log(bool);
+//true
+console.log(nothing);
+//null
+
+//Zadanie 2
+let num1 = 1,
+num2 = 2,
+sum = 0;
+
+sum = num1 + num2;
+
+console.log(sum);
+//3
+
+//Zadanie 3
+let num3;
+console.log(num3);
+//undefined
