d028186d25c8d7bb140372b5860ebc7abe31713a
Dominik Suski <dominiksuski@MacBook-Dominik.local>
2020-10-16 14:49:03 +0200

    03 done

---
+++ 3.Operatory/script.js
@@ -0,0 +1,55 @@
+//Zadanie 1
+let x = true,
+y = false;
+
+console.log(x == y);
+//false
+
+//Zadanie 2
+const num1 = 10,
+num2 = 3;
+
+let moduloResult = 0;
+moduloResult = num1%num2;
+console.log(moduloResult);
+//1
+
+//Zadanie 3
+let stringsResult = "",
+a = "test",
+b = "foo";
+
+stringsResult = a + b;
+console.log(stringsResult);
+//testfoo
+
+//Zadanie 4
+var someNumber = 425;
+var someString = "425";
+
+console.log(someNumber == someString);
+//true
+console.log(someNumber === someString);
+//false
+
+//Zadanie 5
+let counter = 30;
+
+console.log(counter);
+//30
+counter++
+console.log(counter);
+//31
+counter--
+console.log(counter);
+//30
+
+//Zadanie 6
+let n1 = 15,
+n2 = 6,
+result = null;
+
+result = n1>n2;
+
+console.log(result);
+//true
